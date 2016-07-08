9532a36a3e0e2835046d0d79860e23d2896ddb66
Robert Bays <bays.robert@gmail.com>
2016-07-08 16:11:21 +0100

    end of week

--- RobsMVCMovies/RobsMVCMovies/Controllers/HomeController.cs
+++ RobsMVCMovies/RobsMVCMovies/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
 using RobsMVCMovies.Models;
+using System.Net;
 
 namespace RobsMVCMovies.Controllers
 {
@@ -13,41 +14,90 @@ namespace RobsMVCMovies.Controllers
         private MoviesRobEntities db = new MoviesRobEntities();
         
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index(string movieGenre, string searchString)
         {
-            //link query to return all movies from the database
+            var GenreList = new List<string>();
+            var GenreQuery = from g in db.Movies
+                             orderby g.Genre
+                             select g.Genre;
+            GenreList.AddRange(GenreQuery.Distinct());
+            ViewBag.movieGenre = new SelectList(GenreList);
+
+
             var movies = from m in db.Movies
                          select m;
+
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            //Title Search function
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+                        
             
+            //link query to return all movies from the database
+
             return View(movies);
         }
 
         public ActionResult Details(int? Id)
         {
+            if (Id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
             Movie movie = db.Movies.Find(Id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
             return View(movie);
         }
 
         public ActionResult Edit(int? Id)
         {
+            if (Id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
             Movie movie = db.Movies.Find(Id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
             return View(movie);
         }
         
         [HttpPost]
         public ActionResult Edit([Bind(Include = "Id, Title, ReleaseDate, Genre, Price")] Movie movie)
         {//using bind to specify the fields to be returned protects against overposting attacks
-
-            db.Entry(movie).State = EntityState.Modified;
-            db.SaveChanges();
-
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                db.Entry(movie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
+            return View(movie);
         }
 
 
         public ActionResult Delete(int? Id)
         {
+            if(Id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
             Movie movie = db.Movies.Find(Id);
+
+            if(movie == null)
+            {
+                return HttpNotFound();
+            }
             return View(movie);
         }
 
@@ -59,5 +109,26 @@ namespace RobsMVCMovies.Controllers
 
             return RedirectToAction("Index");
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "Title, ReleaseDate, Genre, Price")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movie).State = EntityState.Added;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(movie);
+        }
+
+
+
     }
 }
--- RobsMVCMovies/RobsMVCMovies/Models/Movie.cs
+++ RobsMVCMovies/RobsMVCMovies/Models/Movie.cs
@@ -6,6 +6,8 @@
 //     Manual changes to this file will be overwritten if the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
+using System.ComponentModel.DataAnnotations;
+
 
 namespace RobsMVCMovies.Models
 {
@@ -15,9 +17,24 @@ namespace RobsMVCMovies.Models
     public partial class Movie
     {
         public int Id { get; set; }
+
+        [StringLength(60,MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        
+        [DisplayFormat(DataFormatString = "(0:dd/MM/yyyy)", ApplyFormatInEditMode =true)]
         public Nullable<System.DateTime> ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\-\s]*$")]
+        [Required]
+        [StringLength(30)]
+        
         public string Genre { get; set; }
+
+        [Range(0,1000)]
         public decimal Price { get; set; }
     }
 }
